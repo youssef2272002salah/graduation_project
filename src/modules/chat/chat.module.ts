3fee2246fa640c58596b3ca9b312e92b1b822c80
youssef2272002salah <youssef2272002salah@gmail.com>
2025-04-21 14:59:19 +0200

    Removed backend files and added frontend setup

--- src/modules/chat/chat.module.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { ChatController } from './chat.controller';
-const chatRouter = Router();
-const chatController = new ChatController();
-
-chatRouter.get('/:room/messages',chatController.getMessages);
-
-export { chatRouter };
--- src/modules/users/users.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IUser } from '../users/user.model';
-
-export class UpdateUserDto implements Partial<IUser> {
-  username!: string;
-  email!: string;
-  country!: string;
-  phone!: string;
-  phoneCode!: string;
-}
