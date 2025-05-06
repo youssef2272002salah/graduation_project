5839b5449b56771630efdf58de413619af910ffd
youssef2272002salah <youssef2272002salah@gmail.com>
2025-05-06 11:58:54 +0300

    1

--- src/utils/services/cvAnalysis.ts
+++ src/utils/services/cvAnalysis.ts
@@ -123,7 +123,7 @@ export class ATSAnalysisService {
 
     const completion = await this.groq.chat.completions.create({
       messages: [{ role: "user", content: prompt }],
-      model: "llama3-8b-8192",
+      model: "llama-3.3-70b-versatile",
       temperature: 0.3
     });
 
@@ -176,7 +176,7 @@ export class ATSAnalysisService {
   
     const completion = await this.groq.chat.completions.create({
       messages: [{ role: "user", content: prompt }],
-      model: "llama3-8b-8192",
+      model: "llama-3.3-70b-versatile",
       temperature: 0.3
     });
   
@@ -262,7 +262,7 @@ export class ATSAnalysisService {
     
     const completion = await this.groq.chat.completions.create({
       messages: [{ role: "user", content: prompt }],
-      model: "llama3-8b-8192",
+      model: "llama-3.3-70b-versatile",
       temperature: 0.3
     });
 
@@ -324,7 +324,7 @@ export class ATSAnalysisService {
   
     const completion = await this.groq.chat.completions.create({
       messages: [{ role: "user", content: prompt }],
-      model: "llama3-8b-8192	",
+      model: "llama-3.3-70b-versatile	",
       temperature: 0.3
     });
   
